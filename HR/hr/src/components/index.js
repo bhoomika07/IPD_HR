267e144478c43756e6697bb178557700d672d6e1
Alankrit Arya <alankritarya15@gmail.com>
2022-04-27 14:01:44 +0530

    fixed records

--- HR/hr/src/components/index.js
+++ HR/hr/src/components/index.js
@@ -1,7 +1,7 @@
 export { default as Navbar } from './navbar/Navbar';
 export {default as JobCard} from "./jobcard/JobCard";
 export {default as AptitudeTest} from "./AptitudeTest";
-export {default as Records} from "./Records";
+export {default as Records} from "./records";
 export {default as Confirmation} from "./confirmation";
 export {default as Register} from "./Register";
 export {default as FindCandidates} from "./findCandidates";
