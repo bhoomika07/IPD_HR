341d0ef72c12110f011af59933c05f159b93744b
chaitanya1911 <kchaitanya1911@gmail.com>
2022-04-28 00:45:32 +0530

    added question logic

--- HR/hr/src/components/CreateTest.js
+++ HR/hr/src/components/CreateTest.js
@@ -1,32 +1,120 @@
-import React from 'react'
-import '../styling/findCandidates.css'
+import React, { useState } from "react";
+import "../styling/findCandidates.css";
 
 function CreateTest() {
+  const [quesArr, setQuesArr] = useState([]);
+  const [tempQs, setTempQs] = useState({
+    question: "",
+    option1: "",
+    option2: "",
+    option3: "",
+    option4: "",
+    correctOpt: "",
+  });
+  const handleInput = (e) => {
+    const name = e.target.name;
+    let value = e.target.value;
+    setTempQs({ ...tempQs, [name]: value });
+  };
+  function addQs(e) {
+    e.preventDefault();
+    setQuesArr([...quesArr, tempQs]);
+    setTempQs({
+      question: "",
+      option1: "",
+      option2: "",
+      option3: "",
+      option4: "",
+      correctOpt: "",
+    });
+  }
   return (
-    <div className='container can'>
-        <h4 className='wel'>Create your <b> personalised test!</b></h4>
-        <form>
-        <div className='card'>
-            <div className='card-header'> <input type="text" name="ques" 
-            className="form-control" id="ques" placeholder="Enter your question"/></div>
-            <div className='card-body'>
-            <input type="text" name="op1" 
-            className="form-control" id="op1" placeholder="Enter option 1"/>
-                        <input type="text" name="op2" 
-            className="form-control" id="op2" placeholder="Enter option 2"/>
-                        <input type="text" name="op3" 
-            className="form-control" id="op3" placeholder="Enter option 3"/>
-                        <input type="text" name="op4" 
-            className="form-control" id="op4" placeholder="Enter option 4"/>
-                        <input type="text" name="opr" 
-            className="form-control" id="opr" placeholder="Enter correct option "/>
-            </div>
-            <input type="button" className="btn btn-lg b" value="+ Add Question"/>
+    <div className="container can">
+      <h4 className="wel">
+        Create your <b> personalised test!</b>
+      </h4>
+      <form>
+        <div className="card">
+          <div className="card-header">
+            {" "}
+            <input
+              type="text"
+              name="question"
+              onChange={handleInput}
+              value={tempQs.question}
+              className="form-control"
+              id="ques"
+              placeholder="Enter your question"
+            />
+          </div>
+          <div className="card-body">
+            <input
+              type="text"
+              name="option1"
+              onChange={handleInput}
+              value={tempQs.option1}
+              className="form-control"
+              id="op1"
+              placeholder="Enter option 1"
+            />
+            <input
+              type="text"
+              name="option2"
+              onChange={handleInput}
+              value={tempQs.option2}
+              className="form-control"
+              id="option2"
+              placeholder="Enter option 2"
+            />
+            <input
+              type="text"
+              name="option3"
+              onChange={handleInput}
+              value={tempQs.option3}
+              className="form-control"
+              id="option3"
+              placeholder="Enter option 3"
+            />
+            <input
+              type="text"
+              name="option4"
+              onChange={handleInput}
+              value={tempQs.option4}
+              className="form-control"
+              id="option4"
+              placeholder="Enter option 4"
+            />
+            <input
+              type="text"
+              name="correctOpt"
+              onChange={handleInput}
+              value={tempQs.correctOpt}
+              className="form-control"
+              id="correctOpt"
+              placeholder="Enter correct option "
+            />
+          </div>
+          <input
+            type="button"
+            onClick={(e) => {
+              addQs(e);
+            }}
+            className="btn btn-lg b"
+            value="+ Add Question"
+          />
         </div>
-        </form>
-        <h4><b>Added questions:</b></h4>
-        <p>What is the average of first multiples of five?
-          {/* <span className='row'>
+      </form>
+      <h4>
+        <b>Added questions:</b>
+      </h4>
+      <p>
+        {quesArr.length !== 0 &&
+          quesArr.map((obj) => {
+            return <p>{obj.question}</p>;
+          })}
+        {quesArr.length == 0 && <p>No questions added</p>}
+
+        {/* <span className='row'>
             <span className='col'>
             <input className="btn btn-block btn-lg btn-primary"type="button" value="Edit"/>
             </span>
@@ -34,10 +122,12 @@ function CreateTest() {
             <input className="btn btn-block btn-lg btn-danger"type="button" value="Delete"/>
             </span>
           </span> */}
-          </p>
-        <a href="/confirmation"><input type="button" className="btn btn-lg b" value="Next"/></a>
+      </p>
+      <a href="/confirmation">
+        <input type="button" className="btn btn-lg b" value="Next" />
+      </a>
     </div>
-  )
+  );
 }
 
-export default CreateTest
+export default CreateTest;
--- HR/hr/src/components/findCandidates.js
+++ HR/hr/src/components/findCandidates.js
@@ -1,95 +1,136 @@
-import React from 'react'
-import '../styling/findCandidates.css'
+import React, { useState } from "react";
+import "../styling/findCandidates.css";
+import { useNavigate } from "react-router";
+import axios from "axios";
 function FindCandidates() {
-    
+  const navigate = useNavigate();
+  const [user, setUser] = useState({
+    jobdomain: "",
+    location: "",
+    description: "",
+    minsalary: 0,
+    maxsalary: "",
+    experience: "",
+  });
+
+  //const [records, setRecords] = useState([]);
+
+  const handleInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://127.0.0.1:8000/comp/job/", {
+        data: {
+          compid: "compid69",
+          jobid: "jobid1",
+          date: "2022-04-01",
+          jobdomain: "Software",
+          experience: 7,
+          minsalary: 100,
+          maxsalary: 1000,
+          description: "Description 1",
+          location: "Mumbai",
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        // if (res.data.status_code === 0) {
+        //   navigate("/");
+        // } else {
+        //   alert(res.data.status_msg);
+        // }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    // const newUser = { ...user, id: new Date().getTime().toString() };
+    // setRecords(...records, newUser);
+    // console.log(...records);
+  };
+
   return (
-    <div className='container can'>
-        <h4 className='wel'>Welcome, <b>ABC Co. Ltd.</b> Create a job posting now!</h4>
-        <form>
-        <div className='card'>
-            <div className='card-header'>Enter job title for the open position.</div>
-            <div className='card-body'>
-            <input type="text" name="jobpos"
-            className="form-control" id="jobpos" placeholder="eg: Data Analyst"/>
-            </div>
+    <div className="container can">
+      <h4 className="wel">
+        Welcome, <b>ABC Co. Ltd.</b> Create a job posting now!
+      </h4>
+      <form onSubmit={handleSubmit}>
+        {console.log(user)}
+        <div className="card">
+          <div className="card-header">Enter location.</div>
+          <div className="card-body">
+            <input
+              type="text"
+              name="location"
+              onChange={handleInput}
+              className="form-control"
+              id="location"
+              placeholder="eg: Mumbai, Maharashtra"
+            />
+          </div>
         </div>
-        <div className='card'>
-            <div className='card-header'>Enter location.</div>
-            <div className='card-body'>
-            <input type="text" name="jobloc" 
-            className="form-control" id="jobloc" placeholder="eg: Mumbai, Maharashtra"/>
-            </div>
+        <div className="card">
+          <div className="card-header">Enter min experience.</div>
+          <div className="card-body">
+            <input
+              type="text"
+              name="experience"
+              onChange={handleInput}
+              className="form-control"
+              id="experience"
+              placeholder="eg: 0 or 1"
+            />
+          </div>
         </div>
-        <div className='card'>
-            <div className='card-header'>Enter job description.</div>
-            <div className='card-body'>
-            <input type="textarea" name="jobdesc" 
-            className="form-control" id="jobdesc" placeholder="eg: job: beginner level, need to know python3 and relevant python libraries, etc"/>
-            </div>
+        <div className="card">
+          <div className="card-header">Enter job description.</div>
+          <div className="card-body">
+            <input
+              type="textarea"
+              name="description"
+              onChange={handleInput}
+              className="form-control"
+              id="description"
+              placeholder="eg: job: beginner level, need to know python3 and relevant python libraries, etc"
+            />
+          </div>
         </div>
-        <div className='card'>
-            <div className='card-header'>Enter pay range.</div>
-            <div className='card-body'>
-            <input type="text" name="payrange" 
-            className="form-control" id="payrange" placeholder="eg: 15000"/>
-            </div>
+        <div className="card">
+          <div className="card-header">Enter max pay range.</div>
+          <div className="card-body">
+            <input
+              type="number"
+              name="maxsalary"
+              onChange={handleInput}
+              className="form-control"
+              id="maxsalary"
+              placeholder="eg: 15000"
+            />
+          </div>
         </div>
-        <div className='card'>
-            <div className='card-header'>Select job type.</div>
-            <div className='card-body'>
+        <div className="card">
+          <div className="card-header">Select job type.</div>
+          <div className="card-body">
             <label>
-                Job Role
-            <select>
+              Job Role
+              <select name="jobdomain" onChange={handleInput}>
                 <option value="Data Analyst">Data Analyst</option>
                 <option value="App Developer">App Developer</option>
                 <option value="Web Developer">Web Developer</option>
-            </select>
+              </select>
             </label>
-            </div>
+          </div>
         </div>
-        <div className='card'>
-            <div className='card-header'>Ask for LinkedIn.</div>
-            <div className='card-body'>
-            <div class="custom-control custom-radio">
-                <input type="radio" id="yes" name="customRadio" class="custom-control-input"/>
-                <label className="custom-control-label" htmlFor="customRadio1" > YES </label>
-            </div>
-            <div class="custom-control custom-radio">
-                <input type="radio" id="no" name="customRadio" class="custom-control-input"/>
-                <label className="custom-control-label" htmlFor="customRadio2"> NO </label>
-            </div>
-        </div>
-        </div>
-        <div className='card'>
-            <div className='card-header'>Ask for Candidate CV.</div>
-            <div className='card-body'>
-            <div class="custom-control custom-radio">
-                <input type="radio" id="yes" name="customRadio" class="custom-control-input"/>
-                <label className="custom-control-label" htmlFor="customRadio1" > YES </label>
-            </div>
-            <div class="custom-control custom-radio">
-                <input type="radio" id="no" name="customRadio" class="custom-control-input"/>
-                <label className="custom-control-label" htmlFor="customRadio2"> NO </label>
-            </div>
-        </div>
-        </div>
-        <div className='card'>
-            <div className='card-header'>Create an evaluation test?</div>
-            <div className='card-body'>
-            <div class="custom-control custom-radio">
-                <input type="radio" id="yes" name="customRadio" class="custom-control-input"/>
-                <label className="custom-control-label" htmlFor="customRadio1" > YES </label>
-            </div>
-            <div class="custom-control custom-radio">
-                <input type="radio" id="no" name="customRadio" class="custom-control-input"/>
-                <label className="custom-control-label" htmlFor="customRadio2"> NO </label>
-            </div>
-        </div>
-        </div>
-        </form>
-       <a href="/createTest"><input type="button" className="btn btn-lg b" value="Next"/></a> 
+      </form>
+      <a href="/createTest">
+        <input type="button" className="btn btn-lg b" value="Next" />
+      </a>
     </div>
-  )
+  );
 }
 
-export default FindCandidates
+export default FindCandidates;
